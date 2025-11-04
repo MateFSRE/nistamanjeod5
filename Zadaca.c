cc579675bcbf6ce82535dbfd3ee663fd1ede454a
ZovkoB <151783137+ZovkoB@users.noreply.github.com>
2025-11-04 16:47:30 +0100

    Update Zadaca.c

--- Zadaca.c
+++ Zadaca.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 
 void main(){
-
+	
+	char Prezime[20];
     char Ime[20];
     printf("Napisi Svoje Ime");
     scanf("%s",&Ime);
+    printf("Napisi svoje prezime"); // Bozena pisala
+    scanf("%s",&Prezime);
 }

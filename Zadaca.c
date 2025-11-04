95def284d3485035bd03825b2fe45f2de01e6b92
ivanatokic <151550483+ivanatokic@users.noreply.github.com>
2025-11-04 16:53:37 +0100

    Update Zadaca.c

--- Zadaca.c
+++ Zadaca.c
@@ -4,8 +4,12 @@ void main(){
 	
 	char Prezime[20];
     char Ime[20];
+    int god;
     printf("Napisi Svoje Ime");
     scanf("%s",&Ime);
     printf("Napisi svoje prezime"); // Bozena pisala
     scanf("%s",&Prezime);
+    printf("Napisi koliko godina imas:"); //Ivana pisala
+    scanf("%d",&god);
+    
 }

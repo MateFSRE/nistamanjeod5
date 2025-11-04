eeb1d1bb8b2a91aef154a272cd6640cf40e8050c
gabrijelagrbesicc <gabrijelagrbesic5@gmail.com>
2025-11-04 17:44:12 +0100

    Update

--- Zadaca.c
+++ Zadaca.c
@@ -5,11 +5,13 @@ void main(){
 	char Prezime[20];
     char Ime[20];
     int god;
+    char studij[20];
     printf("Napisi Svoje Ime");
     scanf("%s",&Ime);
     printf("Napisi svoje prezime"); // Bozena pisala
     scanf("%s",&Prezime);
     printf("Napisi koliko godina imas:"); //Ivana pisala
     scanf("%d",&god);
-    
+    printf("Napisi sto studiras: "); //Gabrijela pisala
+    scanf("%s", &studij);
 }

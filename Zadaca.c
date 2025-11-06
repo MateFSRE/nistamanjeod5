8b7b7d5623ef5dd5f516c859f655f7b7201afed7
MateFSRE <mateeraic@gmail.com>
2025-11-06 11:40:32 +0100

    Zavrsetak

--- Zadaca.c
+++ Zadaca.c
@@ -6,12 +6,14 @@ void main(){
     char Ime[20];
     int god;
     char studij[20];
-    printf("Napisi Svoje Ime");
+    printf("Napisi Svoje Ime "); // Mate pisao
     scanf("%s",&Ime);
-    printf("Napisi svoje prezime"); // Bozena pisala
+    printf("Napisi svoje prezime "); // Bozena pisala
     scanf("%s",&Prezime);
     printf("Napisi koliko godina imas:"); //Ivana pisala
     scanf("%d",&god);
     printf("Napisi sto studiras: "); //Gabrijela pisala
     scanf("%s", &studij);
+
+    printf("Vase Ime Je %s %s , Imate %d godina i studirate %s",Ime,Prezime,god,studij); // Mate pisao
 }
